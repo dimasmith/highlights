9579a524eebdfbd60fb9decf51544ffe5f63e4a8
Dmytro Kovalchuk <dimasmith@gmail.com>
2022-10-02 00:11:17 +0300

    feat(model): introduce highlight data model
    The model is an intermediary format for any input and output activities.

---
+++ src/highlights/examples.rs
@@ -0,0 +1,22 @@
+//! Sample data entries to quickly test the system.
+use crate::highlights::{Book, Highlight, Location};
+
+/// Example of a book on chess and life by Garry Kasparov
+pub fn chess_book() -> Book {
+    Book::new(
+        "How Life Imitates Chess: Making the Right Moves, from the Board to the Boardroom",
+        "Garry Kasparov",
+        [
+            Highlight::new(
+                "the reality is that we discard our decisions almost as soon as we make them",
+                Location::new(157, "kindle://book?action=open&asin=B0049U443Q&location=157",
+                ),
+            ),
+            Highlight::new(
+                "Create a personalized map of your decision-making process",
+                Location::new(294, "kindle://book?action=open&asin=B0049U443Q&location=294")),
+            Highlight::new(
+                "The map tells you which areas of your mind are well-known to you and which are still uncharted.",
+                Location::new(295, "kindle://book?action=open&asin=B0049U443Q&location=295"))
+        ])
+}
