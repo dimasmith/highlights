9881d52238c2ae02f8dea9d3136914ffb3f87386
Dmytro Kovalchuk <dimasmith@gmail.com>
2022-10-02 02:02:37 +0300

    feat(cli): support reading input data
    The cli now supports passing input files in a bookcision json format.

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,16 @@
 use std::fs::File;
-use std::io::{stdout, Write};
+use std::io::{stdin, stdout, Read, Write};
 
 pub fn output(out_arg: Option<String>) -> Box<dyn Write> {
     match out_arg {
         Some(path) => Box::new(File::create(path).expect("cannot create file")),
         None => Box::new(stdout()),
+    }
+}
+
+pub fn input(in_arg: Option<String>) -> Box<dyn Read> {
+    match in_arg {
+        Some(path) => Box::new(File::open(path).expect("cannot read file")),
+        None => Box::new(stdin()),
     }
 }
