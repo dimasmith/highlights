3fb1b5b17b51be720d38ba05e070d41dda6710bf
Dmytro Kovalchuk <dimasmith@gmail.com>
2022-10-02 02:02:37 +0300

    feat(cli): support reading input data
    The cli now supports passing input files in a bookcision json format.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use highlights::highlights::examples;
+use highlights::input::bookcision::JsonBook;
+use highlights::input::HighlightsRead;
 use highlights::render::markdown::MarkdownRenderer;
 use highlights::render::Render;
 
 mod io;
 
 fn main() -> std::io::Result<()> {
-    let mut out = io::output(std::env::args().nth(1));
-    let book = examples::chess_book();
+    let mut input = io::input(std::env::args().nth(1));
+    let book = JsonBook::from_reader(&mut input).unwrap().into();
+
+    let mut out = io::output(std::env::args().nth(2));
+
     let mut renderer = MarkdownRenderer::default();
     renderer.render(&book, &mut out)
 }
