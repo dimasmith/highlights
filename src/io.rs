404cf23ba145b68546375210998aac5c3237c9ce
Dmytro Kovalchuk <dimasmith@gmail.com>
2022-10-03 00:15:56 +0300

    feat(error): produce error messages on errors

--- src/io.rs
+++ src/io.rs
@@ -2,16 +2,24 @@ use std::fs::File;
 use std::io::{stdin, stdout, Read, Write};
 use std::path::PathBuf;
 
-pub fn output(out_arg: Option<PathBuf>) -> Box<dyn Write> {
+use highlights::HighlightError;
+
+pub fn output(out_arg: Option<PathBuf>) -> Result<Box<dyn Write>, HighlightError> {
     match out_arg {
-        Some(path) => Box::new(File::create(path).expect("cannot create file")),
-        None => Box::new(stdout()),
+        Some(path) => {
+            let output_file = File::create(path).map_err(HighlightError::from)?;
+            Ok(Box::new(output_file))
+        }
+        None => Ok(Box::new(stdout())),
     }
 }
 
-pub fn input(in_arg: Option<PathBuf>) -> Box<dyn Read> {
+pub fn input(in_arg: Option<PathBuf>) -> Result<Box<dyn Read>, HighlightError> {
     match in_arg {
-        Some(path) => Box::new(File::open(path).expect("cannot read file")),
-        None => Box::new(stdin()),
+        Some(path) => {
+            let input_file = File::open(path).map_err(HighlightError::from)?;
+            Ok(Box::new(input_file))
+        }
+        None => Ok(Box::new(stdin())),
     }
 }
