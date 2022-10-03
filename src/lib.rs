a2247b26110e2c88ec2c264289901c4ff3f8d873
Dmytro Kovalchuk <dimasmith@gmail.com>
2022-10-03 03:05:48 +0300

    docs: add general package documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! Convert kindle highlights to a different formats.
+//!
+//! ## Motivation
+//!
+//! Placing highlights from Kindle to any note taking software requires converting to other formats.
+//! This package helps converting json format exported with the help of [bookcision](https://readwise.io/bookcision)
+//! to Markdown format suitable to placing into Obsidian, Notion and the likes.
+//!
+//! ## Example
+//!
+//! Converting json file to markdown
+//! ```no_run
+//! # use std::fs::File;
+//! # use highlights::input::bookcision::JsonBook;
+//! # use highlights::input::HighlightsRead;
+//! # use highlights::render::markdown::MarkdownRenderer;
+//! # use highlights::render::Render;
+//! #
+//! let input_file = File::open("bookcision.json").unwrap();
+//! let output_file = File::create("highlights.md").unwrap();
+//!
+//! let book = JsonBook::from_reader(input_file).unwrap().into();
+//! let mut renderer = MarkdownRenderer::default();
+//! renderer.render(&book, output_file).unwrap();
+//!
+//! ```
 pub mod error;
 pub mod highlights;
 pub mod input;
